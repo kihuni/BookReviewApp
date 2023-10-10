399496e13d4276f9a7431f0ed003c7e6154085ae
kihuni <stephenkihuni55@gmail.com>
2023-10-10 03:15:41 +0300

    Fixed issues with urls

--- bookreviewplatform/bookreviewplatform/urls.py
+++ bookreviewplatform/bookreviewplatform/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('reviews/', include('reviews.urls')),
+    path('', include('reviews.urls')),
 ]
