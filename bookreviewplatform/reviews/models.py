e6983938f842b5371f2e2736363cf216ecf9e751
kihuni <stephenkihuni55@gmail.com>
2023-10-15 05:28:56 +0300

    modified css

--- bookreviewplatform/reviews/models.py
+++ bookreviewplatform/reviews/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Book(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     description = models.TextField()
