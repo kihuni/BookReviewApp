c497a9980e0b7c0b5135d09cf4315e965fc2eeb4
kihuni <stephenkihuni55@gmail.com>
2023-10-26 18:31:01 +0300

    modified backend

--- bookreviewplatform/start.sh
+++ bookreviewplatform/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Running migrations..."
-python3 /code/bookreviewplatform/manage.py migrate
+python3 /code/manage.py migrate
 echo "Starting gunicorn..."
 gunicorn bookreviewplatform.wsgi:application --bind 0.0.0.0:8000
