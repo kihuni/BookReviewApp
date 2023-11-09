c035364e5eb1cf2fe2e00e0be9f13aadeda7c6d4
kihuni <stephenkihuni55@gmail.com>
2023-11-09 04:29:12 +0300

    modified settings

--- bookreviewplatform/bookreviewplatform/settings.py
+++ bookreviewplatform/bookreviewplatform/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "https://book-review-app-tree-main-bookreview-frontend.vercel.app",
